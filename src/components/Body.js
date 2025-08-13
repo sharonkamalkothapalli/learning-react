2a5fa6d607afd532cae20fb6c763bdf5fa0ec582
Sharon Kamal Kothapalli <sharonkamalkothapalli@gmail.com>
2025-08-13 14:09:08 -0400

    episode-05

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,30 @@
 import { resList } from "../../utils/mockData";
 import RestaurentCard from "./RestaurentCard";
+import { useState } from "react";
 
 const Body = () => {
+  const [listOfRestaurents, setListofRestaurents] = useState(resList);
   return (
     <div className="body">
       <div className="search">Search</div>
+
+      <div className="filer">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredListOfRestaurents = resList.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setListofRestaurents(filteredListOfRestaurents);
+            console.log(filteredListOfRestaurents);
+          }}
+        >
+          Top Rated Restaurents
+        </button>
+      </div>
       <div className="res-container">
         {/* <RestaurentCard resData={resList[0]} /> */}
-        {resList.map((restaurent) => (
+        {listOfRestaurents.map((restaurent) => (
           <RestaurentCard key={restaurent.info.id} resData={restaurent} />
         ))}
       </div>
